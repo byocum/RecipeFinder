1613ffa952367805310959a348f8c68c8ca16616
byocum <byocum1@gmail.com>
2020-07-11 14:38:28 -0400

    Branch2 - Cleanup

--- RecipeFinder/Models/ViewModels/PickIngredientsViewModel.cs
+++ RecipeFinder/Models/ViewModels/PickIngredientsViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
 
 namespace RecipeFinder.Models.ViewModels
 {
@@ -8,7 +6,6 @@ namespace RecipeFinder.Models.ViewModels
     {
         public string SelectedIngredient { get; set; }
 
-        //These are List instead of IEnumerable because IEnumerable does not have an index whereas List does.
         public List<Ingredient> Ingredients { get; set; }
 
         public List<Ingredient> SelectedIngredients { get; set; }
