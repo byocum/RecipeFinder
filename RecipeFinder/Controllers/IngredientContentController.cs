397a93bb6f483ecef2ca290a7cbbb790423e4760
byocum <byocum1@gmail.com>
2021-02-13 12:21:20 -0500

    Added AddContent Index page and started on Ingredient content list page.

---
+++ RecipeFinder/Controllers/IngredientContentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeFinder.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeFinder.Controllers
+{
+    public class IngredientContentController : Controller
+    {
+        private readonly RecipeFinderContext _context;
+
+        public IngredientContentController(RecipeFinderContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> IngredientContentList()
+        {
+            Models.ViewModels.IngredientsListViewModel IngredientsListViewModel = new Models.ViewModels.IngredientsListViewModel();
+            IngredientsListViewModel.Ingredients = await _context.Ingredients
+                .OrderBy(i => i.IngredientNameId)
+                .ToListAsync();
+            return View(IngredientsListViewModel);
+        }
+    }
+}
---
+++ RecipeFinder/Models/ViewModels/IngredientsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeFinder.Models.ViewModels
+{
+    public class IngredientsListViewModel
+    {
+        public List<Ingredient> Ingredients { get; set; }
+    }
+}
